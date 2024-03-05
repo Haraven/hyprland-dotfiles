27076b328fb4c81d5e43b946e8d8b34aa0d76bf5
Radu Corbu <radu.g.corbu@gmail.com>
2024-03-05 18:36:29 +0200

    refactor: improve update_config.sh readability

--- Scripts/update_configs.sh
+++ Scripts/update_configs.sh
@@ -8,102 +8,101 @@ repo_home=$HOME/hyprland-dotfiles
 repo_configs=$repo_home/Configs
 user_configs=$HOME/.config
 
+echo "Copying settings:"
+
 # VS Code
-echo "Copying VS Code keybindings..."
+echo "vscode..."
 cp $user_configs/Code/User/keybindings.json $repo_configs/.config/Code/User/
-echo "Copying VS Code settings..."
 cp $user_configs/Code/User/settings.json $repo_configs/.config/Code/User/
 
 # Dunst
-echo "Copying Dunst settings..."
+echo "dunst..."
 cp -r $user_configs/dunst/ $repo_configs/.config/
 
 # Fish
-echo "Copying Fish settings..."
+echo "fish..."
 cp $user_configs/fish/config.fish $repo_configs/.config/fish/
 
 # GTK-3
-echo "Copying GTK-3 bookmarks..."
+echo "gtk-3..."
 cp $user_configs/gtk-3.0/bookmarks $repo_configs/.config/gtk-3.0/
-echo "Copying GTK-3 settings..."
 cp $user_configs/gtk-3.0/settings.ini $repo_configs/.config/gtk-3.0/
 
 # Hyprland
-echo "Copying Hyprland configs..."
+echo "hyprland..."
 cp -r $user_configs/hypr/ $repo_configs/.config/
 
 # Kitty
-echo "Copying Kitty themes..."
+echo "kitty..."
 cp -r $user_configs/kitty/themes/ $repo_configs/.config/kitty/
-echo "Copying Kitty config..."
 cp $user_configs/kitty/kitty.conf $repo_configs/.config/kitty/
 
 # Kvantum
-echo "Copying Kvantum configs..."
+echo "kvantum..."
 cp -r $user_configs/Kvantum/ $repo_configs/.config/
 
 # LSD
-echo "Copying LSD settings..."
+echo "lsd..."
 cp -r $user_configs/lsd/ $repo_configs/.config/
 
 # neofetch
-echo "Copying neofetch settings..."
+echo "neofetch..."
 cp -r $user_configs/neofetch/ $repo_configs/.config/
 
 # nwg-look
-echo "Copying nwg-look settings..."
+echo "nwg-look..."
 cp $user_configs/nwg-look/config $repo_configs/.config/nwg-look/
 
 # Octopi
-echo "Copying Octopi settings..."
+echo "octopi..."
 cp $user_configs/octopi/octopi.conf $repo_configs/.config/octopi/
 
 # qt5ct
-echo "Copying qt5ct settings..."
+echo "qt5ct..."
 cp -r $user_configs/qt5ct/ $repo_configs/.config/
 
 # qt6ct
-echo "Copying qt6ct settings..."
+echo "qt6ct..."
 cp -r $user_configs/qt6ct/ $repo_configs/.config/
 
 # Rofi
-echo "Copying Rofi settings..."
+echo "rofi..."
 cp -r $user_configs/rofi/ $repo_configs/.config/
 
 # Swaylock
-echo "Copying Swaylock settings..."
+echo "swaylock..."
 cp $user_configs/swaylock/config $repo_configs/.config/swaylock/
 
 # swww
-echo "Copying swww settings..."
+echo "swww..."
 cp -r $user_configs/swww/ $repo_configs/.config/
 
 # waybar
-echo "Copying waybar settings..."
+echo "waybar..."
 cp -r $user_configs/waybar/ $repo_configs/.config/
 
 # wlogout
-echo "Copying wlogout settings..."
+echo "wlogout..."
 cp -r $user_configs/wlogout/ $repo_configs/.config/
 
 # xdg-desktop-portal
-echo "Copying xdg-desktop-portal settings..."
+echo "xdg-desktop-portal..."
 cp $user_configs/xdg-desktop-portal/portals.conf $repo_configs/.config/xdg-desktop-portal/
 
 # xsettingsd
-echo "Copying xsettingsd settings..."
+echo "xsettingsd..."
 cp $user_configs/xsettingsd/xsettingsd.conf $repo_configs/.config/xsettingsd
 
 # Dolphin
-echo "Copying dolphinrc..."
+echo "copying dolphinrc..."
 cp $user_configs/dolphinrc $repo_configs/.config/
 
 # kdeglobals
-echo "Copying kdeglobals..."
+echo "copying kdeglobals..."
 cp $user_configs/kdeglobals $repo_configs/.config/
 
 # Starship
-echo "Copying starship settings..."
+echo "starship..."
 cp $user_configs/starship.toml $repo_configs/.config/
 
 # ----------------------------------------------------------------
@@ -123,16 +122,17 @@ cp $user_icons/default/index.theme $repo_configs/.icons/default/
 # ----------------------------------------------------------------
 
 echo "============================================================"
+echo "Copying local settings..."
 
 user_shared_locals=$HOME/.local/share
 
 # Dolphin
-echo "Copying Dolphin shared settings..."
+echo "dolphin..."
 cp -r $user_shared_locals/dolphin/ $repo_configs/.local/share/
 cp -r $user_shared_locals/kxmlgui5/ $repo_configs/.local/share/
 
 # Fonts
-echo "Copying shared fonts..."
+echo "fonts..."
 cp -r /usr/share/fonts/noto/ $repo_configs/.local/share/fonts/
 cp -r /usr/share/fonts/noto-nerd-font/ $repo_configs/.local/share/fonts/
 
